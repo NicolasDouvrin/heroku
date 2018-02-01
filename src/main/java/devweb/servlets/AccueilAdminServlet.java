ee8deb2692bb6b73e0d830589cf5e94290e4bb81
Nicolas Douvrin <nicolas.douvrin@hei.yncrea.fr>
2018-02-01 12:17:28 +0100

    maj 9 mobile

--- src/main/java/devweb/servlets/AccueilAdminServlet.java
+++ src/main/java/devweb/servlets/AccueilAdminServlet.java
@@ -1,4 +1,25 @@
 package devweb.servlets;
 
-public class AccueilAdminServlet {
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/accueiladmin")
+public class AccueilAdminServlet extends GenericServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setCharacterEncoding("UTF-8");
+
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+
+        TemplateEngine templateEngine = createTemplateEngine(req.getServletContext());
+        templateEngine.process("accueilAdmin", context, resp.getWriter());
+    }
 }
